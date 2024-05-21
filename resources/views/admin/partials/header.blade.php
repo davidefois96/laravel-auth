79914ddf2edca854526f50de09dcdccf8eab0f6e
davidefois96 <davidefois360@gmail.com>
2024-05-21 22:45:11 +0200

    [add] link to route admin.home inside navbar

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -2,6 +2,7 @@
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
           <a href="{{route('home')}}" target="_blank" class="navbar-brand">Vai al sito</a>
+          <a href="{{route('admin.home')}}"  class="navbar-brand">Home</a>
           <form action="{{route('logout')}}" method="POST" class="d-flex">
             <p class="me-3">{{Auth::user()->name}}</p>
            @csrf
